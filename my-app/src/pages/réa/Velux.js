b7ef146d0d11c5945bc5d63f1dbd51a2c60d6379
LucasL72 <lucasj.ledoux@gmail.com>
2022-08-19 11:55:03 +0200

    fix images & texts

--- my-app/src/pages/réa/Velux.js
+++ my-app/src/pages/réa/Velux.js
@@ -53,23 +53,7 @@ const Velux = () => {
               </Col>
               <Col md={12} className="mt-3">
                 <p className="justif">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Feugiat scelerisque varius morbi enim nunc faucibus a
-                  pellentesque sit. Volutpat ac tincidunt vitae semper. Vitae
-                  semper quis lectus nulla at volutpat. Elit duis tristique
-                  sollicitudin nibh sit amet commodo nulla facilisi. In ornare
-                  quam viverra orci sagittis eu volutpat. Risus nullam eget
-                  felis eget. Et netus et malesuada fames ac turpis egestas.
-                  Egestas fringilla phasellus faucibus scelerisque eleifend
-                  donec. Viverra orci sagittis eu volutpat. Sed egestas egestas
-                  fringilla phasellus faucibus scelerisque. Tempus egestas sed
-                  sed risus pretium. Mattis nunc sed blandit libero. Euismod in
-                  pellentesque massa placerat duis ultricies lacus sed.
-                  Scelerisque eu ultrices vitae auctor. Massa placerat duis
-                  ultricies lacus sed turpis. Elementum nibh tellus molestie
-                  nunc. Commodo sed egestas egestas fringilla phasellus
-                  faucibus. Tristique magna sit amet purus gravida quis blandit.
+    
                 </p>
               </Col>
             </Row>
