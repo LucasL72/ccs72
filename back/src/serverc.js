b8b9d912e0788beb8ec7934b89b700587f7a0c7c
lucas <lucas@vps-bf4d6f66.vps.ovh.net>
2022-08-19 09:14:54 +0000

    server 190822

--- back/src/serverc.js
+++ back/src/serverc.js
@@ -20,7 +20,6 @@ class Server {
           "https://charpente72.fr",
           "http://www.charpente72.fr",
           "http://charpente72.fr",
-          "http://localhost:3000",
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
@@ -43,7 +42,7 @@ class Server {
     );
 
     // Express static permet de diriger un chemin sur un dossier en particulier
-    this.app.use("/apiccs/assets", express.static("public"));
+    this.app.use("/api/assets", express.static("public"));
     // Routes
     this.app.use(router);
     // Run app
